0fe69daad9e83c5e80c8dc2fb60b1269a4eed77a
Akinbohun Ifedayo <111973534+deeygitalstudio@users.noreply.github.com>
2022-11-23 15:57:27 +0100

    Update index.js

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,6 +1,8 @@
 const keysEl = document.getElementsByTagName("li");
 const resultEl = document.querySelector("p");
-const eraseEl = document.querySelector(".erase")
+const eraseEl = document.querySelector(".erase");
+const hourEl = document.querySelector('.hour');
+const secondsEl  = document.querySelector('.seconds');
 
  for (let i = 0; i < keysEl.length; i++){
     if(keysEl[i].innerHTML === "="){
@@ -30,3 +32,22 @@ function calculate() {
 eraseEl.addEventListener("click", () => {
     resultEl.innerHTML = "";
 })
+
+
+const updateTime = () => {
+    const currentTime = new Date();
+
+    let currentHour = currentTime.getHours();
+    const currentMinute = currentTime.getMinutes();
+
+    if (currentHour > 12) {
+        currentHour -= 12;
+    }
+
+
+    hourEl.textContent = currentHour.toString();
+    secondsEl.textContent = currentMinute.toString().padStart(2, '0');
+
+} 
+setInterval(updateTime, 1000);
+updateTime();
