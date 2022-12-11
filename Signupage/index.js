c5de123e4cf6a979a33f635d784f3d7987b57fc1
Akinbohun Ifedayo <111973534+deeygitalstudio@users.noreply.github.com>
2022-12-11 10:16:20 +0100

    Update index.js

--- Signupage/index.js
+++ Signupage/index.js
@@ -1,1 +1,31 @@
 
+const loginEl = document.querySelector("#login");
+const backEl = document.querySelector("#back");
+const resetEl = document.querySelector("#reset");
+const forgetEl = document.querySelector("#forget");
+const loginformEl = document.querySelector(".loginform");
+const signupEl = document.querySelector("#signup");
+const signupformEl = document.querySelector("#signupform");
+
+loginEl.addEventListener("click", () => {	
+signupformEl.style.display = "block";
+loginformEl.style.display = "none";
+
+
+})
+
+signupEl.addEventListener("click", () => {	
+   signupformEl.style.display = "none";
+   loginformEl.style.display = "block";
+})
+
+forgetEl.addEventListener("click", () => {
+   loginformEl.style.display = "none";
+   signupformEl.style.display = "none";
+   resetEl.style.display = "block";
+})
+
+backEl.addEventListener("click", () => {
+   resetEl.style.display = "none";
+   loginformEl.style.display = "block";
+})
