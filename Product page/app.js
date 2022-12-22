74d7ef2c3aa31d16ed1889264bb5b2bb95b548b2
Akinbohun Ifedayo <111973534+deeygitalstudio@users.noreply.github.com>
2022-12-22 12:35:16 +0100

    Update app.js

--- Product page/app.js
+++ Product page/app.js
@@ -1,7 +1,6 @@
-
 window.onload = () => {
     swal("This project is created by akinbohun Ifedayo, just to test my basic javascript skills")
-}
+};
 
 const countEL = document.getElementById("count");
 const increaseEL = document.getElementById("increase");
@@ -46,7 +45,7 @@ btnEL.addEventListener('click', () => {
     if(countEL.textContent <= 0){
         swal('Please select the amount you want to buy')
     }else{
-        swal('countEL.textContent +   'item(s) has been added to cart and the amount is ' + amountEL.innerHTML)
+        swal(countEL.textContent +   'item(s) has been added to cart and the amount is ' + amountEL.innerHTML)
     }
 });
 
