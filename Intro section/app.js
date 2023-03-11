b67e97f4156c9d88bc253e8f6d4018ec6233efd9
Adeola-creator <111973534+deeygitalstudio@users.noreply.github.com>
2023-03-11 12:24:32 +0100

    Into Section
    Simple JS projects

---
+++ Intro section/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+ $('#btn1').click(function(){
+    $('#features').slideToggle('slow');
+ })
+ $('#btn2').click(function(){
+    $('#company').slideToggle('slow');
+ })
+})
