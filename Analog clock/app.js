b9f1b41819bb70d6bb0f6b1eb7750cf8a1387323
Akinbohun Ifedayo <111973534+deeygitalstudio@users.noreply.github.com>
2023-05-21 15:52:57 +0100

    Update app.js

--- Analog clock/app.js
+++ Analog clock/app.js
@@ -1,6 +1,10 @@
 const hourEl = document.querySelector('.hour')
 const secondEl = document.querySelector('.seconds')
 const minutesEl = document.querySelector('.minutes')
+const hrsEl = document.querySelector('.hrs')
+const minsEl = document.querySelector('.mins')
+const secsEl = document.querySelector('.secs')
+
 
 function updateClock(){
 const currentDate = new Date()
@@ -8,6 +12,7 @@ const currentDate = new Date()
 const hour = currentDate.getHours()
 const minutes= currentDate.getMinutes()
 const seconds = currentDate.getSeconds()
+const ampmEl = document.querySelector('.ampm')
 console.log(hour, minutes, seconds);
 
 const hourDeg = (hour / 12) * 360;
@@ -17,7 +22,18 @@ minutesEl.style.transform = `rotate(${minuteDeg}deg)`;
 const secondDeg = (seconds / 60) * 360;
 secondEl.style.transform = `rotate(${secondDeg}deg)`;
 
+hrsEl.innerHTML = hour % 12 + " : " ;
+minsEl.innerHTML = minutes + " :";
+secsEl.innerHTML = seconds; 
+
+if(hour >= 12){
+  ampmEl.innerHTML = "PM"
+}else{
+    ampmEl.innerHTML = "AM"
 }
+
+}
+
 
 
 setInterval(updateClock, 1000)
